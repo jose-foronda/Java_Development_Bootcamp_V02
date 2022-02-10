9d573529f4c91c06f7717d461e653cf02f667f03
jose-foronda <jose09621@hotmail.com>
2022-02-10 17:24:07 -0500

    Editing of the Java source code files.

--- Clases_bootcamp/practicas/practica-semanal-semana2/src/DateValidation.java
+++ Clases_bootcamp/practicas/practica-semanal-semana2/src/DateValidation.java
@@ -1,5 +1,11 @@
 
 public class DateValidation 
 {
-
+	public void validatorOfDates(String date) 
+	{
+		String[] dateStringArray = date.split("/");
+		
+		String day = dateStringArray[0];
+		
+	}
 }
--- Clases_bootcamp/practicas/practica-semanal-semana2/src/TestDateValidation.java
+++ Clases_bootcamp/practicas/practica-semanal-semana2/src/TestDateValidation.java
@@ -4,7 +4,9 @@ public class TestDateValidation
 
 	public static void main(String[] args) 
 	{
-
+		String testDateString = "10/02/2022";
+		
+		DateValidation dateValidator = new DateValidation();
 	}
 
 }
