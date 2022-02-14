c69dba68d357e03f37b9ed92a7ee440c70b1a074
jose-foronda <jose09621@hotmail.com>
2022-02-13 22:01:54 -0500

    Solution of programming exercise to validate dates in format  dd/mm/yyyy (version 1.0)

--- Clases_bootcamp/practicas/practica-semanal-semana2/src/DateValidation.java
+++ Clases_bootcamp/practicas/practica-semanal-semana2/src/DateValidation.java
@@ -1,11 +1,71 @@
 
+
 public class DateValidation 
-{
-	public void validatorOfDates(String date) 
+{	
+	/* Fields section */
+	private final short MIN_YEAR = 1900;
+	private final short MAX_YEAR = 2099;
+	
+	private final byte MIN_MONTH = 1;
+	private final byte MAX_MONTH = 12;
+	
+	private final byte MIN_DAY = 1;
+	
+	private final byte[] MAX_DAYS_NORMAL_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	
+	/* Methods Section*/
+	public boolean validateDate(String date) 
 	{
 		String[] dateStringArray = date.split("/");
+ 
+//		String day = dateStringArray[0];
+//		String month = dateStringArray[1];
+//		String yearString = dateStringArray[2]; 
 		
-		String day = dateStringArray[0];
+		if ( !validateYear(dateStringArray[2]) )  
+			return false; 
 		
+		if ( !validateMonth(dateStringArray[1]) )  
+			return false; 
+		
+		return validateDay(dateStringArray[0], dateStringArray[1], dateStringArray[2]); 
+	}
+	
+	public boolean validateDay(String dayString, String monthString, String yearString)
+	{
+		short yearValue = Short.parseShort(yearString);
+		byte monthValue = Byte.parseByte(monthString);
+		byte dayValue = Byte.parseByte(dayString);
+		
+//		Año bisiesto es el divisible entre 4, salvo que sea año secular -último de cada siglo, 
+//		terminado en «00»-, en cuyo caso también ha de ser divisible entre 400.
+		if ( (yearValue % 4 == 0) ) 
+		{
+			//año terminado en 00 que es bisiesto
+			if ( (yearValue % 100 == 0) )
+				if ((yearValue % 400 == 0)) 
+					return ( dayValue >=  this.MIN_DAY && dayValue <= (this.MAX_DAYS_NORMAL_MONTH[ monthValue - 1 ] + 1) );
+				else 
+					return ( dayValue >= this.MIN_DAY && dayValue <= (this.MAX_DAYS_NORMAL_MONTH[ monthValue - 1 ]) );
+			else
+				return ( dayValue >=  this.MIN_DAY && dayValue <= (this.MAX_DAYS_NORMAL_MONTH[ monthValue - 1 ] + 1) );
+		}
+		else 
+		{
+			return ( dayValue >=  this.MIN_DAY && dayValue <= this.MAX_DAYS_NORMAL_MONTH[ monthValue - 1 ] );
+		}
+	}
+	
+	public boolean validateMonth(String monthString)
+	{
+		byte monthValue = Byte.parseByte(monthString);
+		return (monthValue >= this.MIN_MONTH) && (monthValue <= this.MAX_MONTH);
+	}
+	
+	public boolean validateYear(String yearString)
+	{
+		short yearValue = Short.parseShort(yearString);
+		return (yearValue >= this.MIN_YEAR) && (yearValue <= this.MAX_YEAR);
 	}
 }
--- Clases_bootcamp/practicas/practica-semanal-semana2/src/TestDateValidation.java
+++ Clases_bootcamp/practicas/practica-semanal-semana2/src/TestDateValidation.java
@@ -1,12 +1,48 @@
+import java.util.Scanner;
 
 public class TestDateValidation 
 {
-
 	public static void main(String[] args) 
 	{
-		String testDateString = "10/02/2022";
+		// Exercise
+//		"Crear un brach con el nombre practica-semanal-semana2:
+//	     Realizar el ejercicio validación de fechas:
+//	     Ingresar una fecha dd/mm/yyyy
+//	     validar que dd >=1 && <=31 TENIENDO EN CUENTA EL MES
+//	     validar que mes >=1 && <=12
+//	     validar que anio >=1900 && <= 2099"
+		
+		//enero tiene 31 siempre.
+		//febrero tiene normalmente 28, y 29 cuando el numero de años es divisible x4
+		//marzo tiene 31 siempre.
+		//abril tiene 30 siempre.
+		//mayo tiene 31 siempre.
+		//junio tiene 30 siempre.
+		//Julio tiene 31 siempre.
+		//agosto tiene 31 siempre.
+		//septiembre tiene 30 siempre.
+		//octubre tiene 31 siempre.
+		//noviembre tiene 30 siempre.
+		//diciembre tiene 31 siempre.
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		String testDateString = "";
+		
+		do {
+			System.out.println("Introduce a date with the format DD/MM/YYYY  (Valid years are >= 1900 and <=2099) :  ");
+//			testDateString = "28/02/2099";
+			testDateString = userInput.next();
+			
+		} while ( !(testDateString.split("/").length == 3) );
+		
+		userInput.close();
+		
 		
 		DateValidation dateValidator = new DateValidation();
+		
+		String validation = dateValidator.validateDate(testDateString) ? "VALID" : "INVALID";
+		
+		System.out.println( "the date " + testDateString +" is " + validation );
 	}
-
 }
