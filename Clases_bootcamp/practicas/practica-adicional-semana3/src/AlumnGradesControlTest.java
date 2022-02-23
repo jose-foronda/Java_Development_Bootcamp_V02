2f664116f67be136bcb6c109e652e9e7dbdef3ab
jose-foronda <jose09621@hotmail.com>
2022-02-22 22:33:20 -0500

    I changed the code of the scanner. I changed .next() with .nextLine() and addes .nextLine() after use each method of the Scanner class.

--- Clases_bootcamp/practicas/practica-adicional-semana3/src/AlumnGradesControlTest.java
+++ Clases_bootcamp/practicas/practica-adicional-semana3/src/AlumnGradesControlTest.java
@@ -1,26 +1,24 @@
 import java.util.ArrayList;  
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class AlumnGradesControlTest {
 
 	public static void main(String[] args) 
 	{
-//		"Crear un branch con el nombre practica-adicional-semana3:
-//		 La idea es practicar ciclos, dobles con vectores, máximo, mínimo, promedio.
-//		 realizar el siguiente ejercicios:
-//		 La escuela primaria xyz solicita el desarrollo de una aplicación para poder llevar el control de notas para cada alumno.
-//		 El programa solicitará el ingreso la cantidad de alumnos a procesar.
-//		 Para cada alumno se deberá ingresar los siguientes datos:
-//		 - nombre (string)
-//		 - apellido (string)
-//		 - número de examen
-//		 - nota que obtubo el alumno (númerico >=0 <=10)
-//		 Al finalizar la carga de datos, se procederá a listar la siguiente información:
-//		 1- alumno/s con la calificación mas alta
-//		 2- alumno/s con la calificación mas baja
-//		 3- alumnos que promocionan, aquellos que obtubieron un promerdio >= 7
-//		 4- alumnos que deben recursar la materia, aquellos que obtuvieron un promedio < 7"""
-		
+		/*			*** Exercise week three ***
+		 * // "Crear un branch con el nombre practica-adicional-semana3: // La idea es
+		 * practicar ciclos, dobles con vectores, máximo, mínimo, promedio. // realizar
+		 * el siguiente ejercicios: // La escuela primaria xyz solicita el desarrollo de
+		 * una aplicación para poder llevar el control de notas para cada alumno. // El
+		 * programa solicitará el ingreso la cantidad de alumnos a procesar. // Para
+		 * cada alumno se deberá ingresar los siguientes datos: // - nombre (string) //
+		 * - apellido (string) // - número de examen // - nota que obtubo el alumno
+		 * (númerico >=0 <=10) // Al finalizar la carga de datos, se procederá a listar
+		 * la siguiente información: // 1- alumno/s con la calificación mas alta // 2-
+		 * alumno/s con la calificación mas baja // 3- alumnos que promocionan, aquellos
+		 * que obtubieron un promerdio >= 7 // 4- alumnos que deben recursar la materia,
+		 * aquellos que obtuvieron un promedio < 7"""
+		 */		
 		
 		//user input
 		Scanner userInputScanner = new Scanner(System.in);
@@ -32,11 +30,13 @@ public class AlumnGradesControlTest {
 		
 		System.out.print("Please enter the NUMBER OF ALUMNS to process: ");
 		byte alumnNumber = userInputScanner.nextByte();
+		userInputScanner.nextLine();
 		
 //		System.out.println();
 //		System.out.println();
 		System.out.print("Please enter the NUMBER OF TESTS per-alumn to process: ");
 		byte testNumber = userInputScanner.nextByte();
+		userInputScanner.nextLine();
 		
 		ArrayList<Alumno> alumnList = new ArrayList<Alumno>();
 		
@@ -48,22 +48,26 @@ public class AlumnGradesControlTest {
 			System.out.println();
 			System.out.println();
 			System.out.print("Please enter the FIRST-NAME of the ALUMN #" + alumnCounter + " : ");
-			String AlumnfirstName = userInputScanner.next();
+			String AlumnfirstName = userInputScanner.nextLine();
+//			userInputScanner.nextLine();
 			
 			System.out.print("Please enter the LAST-NAME of the ALUMN #" + alumnCounter + " : ");
-			String AlumnLastName = userInputScanner.next();
-			
+			String AlumnLastName = userInputScanner.nextLine();
+//			userInputScanner.nextLine();
+						
 			Test[] alumnTests = new Test[testNumber];
 			for (int i = 0; i < testNumber; i++) 
 			{
 				System.out.print("Please enter the TEST-NUMBER for the ALUMN #" + alumnCounter + " : ");		
 				byte testId = userInputScanner.nextByte();
+				userInputScanner.nextLine();
 				
 				float testGrade = 0;
 				do 
 				{
 					System.out.print("Please enter the GRADE ( 0=< grade <= 10) obtained for the TEST " + testId + " for the ALUMN #" + alumnCounter + " : ");		
 					testGrade = Float.parseFloat(userInputScanner.next());
+					userInputScanner.nextLine();
 				} while ( !((testGrade >= 0) && (testGrade <= 10)) );
 
 				
